33c5c46f9229f8d8e6d514982a38f09dfa8ebea7
金飞 <jin.fei@zuodashi.com>
2019-07-04 14:51:47 +0800

    s

--- http/post.go
+++ http/post.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -11,7 +12,7 @@ import (
 // 发送POST请求
 // url:请求地址，data:POST请求提交的数据,contentType:请求体格式，如：application/json
 // content:请求放回的内容
-func Post(url string, data interface{}, contentType string, headers map[string]string) (content string) {
+func Post(url string, data interface{}, contentType string, headers map[string]string, insecureSkipVerify bool) (content string) {
 	jsonStr, _ := json.Marshal(data)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Add("content-type", contentType)
@@ -23,7 +24,15 @@ func Post(url string, data interface{}, contentType string, headers map[string]s
 	}
 	defer req.Body.Close()
 
-	client := &http.Client{Timeout: 5 * time.Second}
+	//跳过证书验证
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: insecureSkipVerify},
+	}
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: tr,
+	}
 	resp, error := client.Do(req)
 	if error != nil {
 		panic(error)
